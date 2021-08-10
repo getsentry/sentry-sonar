f408c8866551e4b6b31940b9cc514ac49b7782bc
Jan Michael Auer <mail@jauer.org>
2021-08-10 11:32:46 +0200

    Remove unused example plugin initialization

--- src/main/java/io/sentry/integrations/sonar/SentryPlugin.java
+++ src/main/java/io/sentry/integrations/sonar/SentryPlugin.java
@@ -21,17 +21,7 @@ package io.sentry.integrations.sonar;
 
 import io.sentry.integrations.sonar.rules.*;
 import org.sonar.api.Plugin;
-import io.sentry.integrations.sonar.hooks.PostJobInScanner;
-import io.sentry.integrations.sonar.hooks.DisplayQualityGateStatus;
-import io.sentry.integrations.sonar.languages.FooLanguage;
-import io.sentry.integrations.sonar.languages.FooQualityProfile;
-import io.sentry.integrations.sonar.measures.ComputeSizeAverage;
-import io.sentry.integrations.sonar.measures.ComputeSizeRating;
-import io.sentry.integrations.sonar.measures.ExampleMetrics;
-import io.sentry.integrations.sonar.measures.SetSizeOnFilesSensor;
-import io.sentry.integrations.sonar.settings.FooLanguageProperties;
 import io.sentry.integrations.sonar.settings.SentryProperties;
-import io.sentry.integrations.sonar.settings.SayHelloFromScanner;
 
 /**
  * This class is the entry point for all extensions. It is referenced in pom.xml.
@@ -40,25 +30,7 @@ public class SentryPlugin implements Plugin {
 
     @Override
     public void define(Context context) {
-        // tutorial on hooks
-        // http://docs.sonarqube.org/display/DEV/Adding+Hooks
-        // context.addExtensions(PostJobInScanner.class, DisplayQualityGateStatus.class);
-
-        // tutorial on languages
-        // context.addExtensions(FooLanguage.class, FooQualityProfile.class);
-        // context.addExtensions(FooLanguageProperties.getProperties());
-
-        // tutorial on measures
-        // context.addExtensions(ExampleMetrics.class, SetSizeOnFilesSensor.class, ComputeSizeAverage.class, ComputeSizeRating.class);
-
-        // tutorial on rules
-        // context.addExtensions(JavaRulesDefinition.class, CreateIssuesOnJavaFilesSensor.class);
-        // context.addExtensions(FooLintRulesDefinition.class, FooLintIssuesLoaderSensor.class);
-
         context.addExtensions(SentryRulesDefinition.class, CreateSentryIssuesSensor.class);
         context.addExtensions(SentryProperties.getProperties());
-
-        // tutorial on settings
-        //   .addExtension(SayHelloFromScanner.class);
     }
 }
